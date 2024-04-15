4c8fbf24af3756e06b239bd718299dbb98423dd0
Igor Lunyakin <lunyaigor484@gmail.com>
2024-04-16 01:57:55 +0300

    change imports

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from 'src/modules/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import configurations from 'src/configurations';
+import configurations from '../../configurations';
 import { User } from '../users/models/user.model';
 
 @Module({
